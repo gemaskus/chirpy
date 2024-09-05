61aa25ed00ccd651560be9a4fccbc36f51462287
Grant <gemaskus@icloud.com>
2024-09-04 21:24:59 -0600

    chirpy webserver chapter 3 lesson 4

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,15 +10,14 @@ func main() {
 	const filepathRoot = "."
 
 	config := &apiConfig{
-		fileserverHits: 0,
+		FileserverHits: 0,
 	}
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", config.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer((http.Dir(filepathRoot))))))
 	mux.HandleFunc("GET /api/healthz", config.handlerReadiness)
-	mux.HandleFunc("GET /api/metrics", config.handlerHits)
+	mux.HandleFunc("GET /admin/metrics", config.handlerHits)
 	mux.HandleFunc("GET /api/reset", config.handlerReset)
-	//mux.HandleFunc("POST /healthz", handlerHealthzPost)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -31,17 +29,5 @@ func main() {
 }
 
 type apiConfig struct {
-	fileserverHits int
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits++
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) handlerHits(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hits: %d\n", cfg.fileserverHits)
+	FileserverHits int
 }
